3000c4ccdced5c216a15125ba973f7b7810f9ea5
saunlani <saunlani@gmail.com>
2018-06-12 20:57:10 -0400

    first commit

--- ajax_lab/Controllers/HomeController.cs
+++ ajax_lab/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ajax_lab.Models;
 
 namespace ajax_lab.Controllers
 {
@@ -13,17 +14,89 @@ namespace ajax_lab.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Search()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
-        public ActionResult Contact()
+        //combining author an
+        [HttpPost]
+        public ActionResult GetBooksByAuthor(string author)
         {
-            ViewBag.Message = "Your contact page.";
+            Library2Entities db = new Library2Entities();
 
+            List<book> list = db.books
+                .Where(c => c.Author.Contains(author)).ToList();
+
+            return Json(list);
+        }
+
+
+        [HttpPost]
+        public ActionResult GetBooksByTitle(string title)
+        {
+            Library2Entities db = new Library2Entities();
+
+            List<book> list = db.books.Where(
+               c => c.Title.Contains(title)
+                ).ToList();
+
+            return Json(list);
+        }
+
+        public ActionResult TitleSearch()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult GetBooksByYear(int ?year)
+        {
+            Library2Entities db = new Library2Entities();
+
+            List<book> list = db.books.Where(
+               c => c.YearPublished == (year)
+                ).ToList();
+
+            return Json(list);
+        }
+
+        public ActionResult YearSearch()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult GetBooksByPublisher(string publisher)
+        {
+            Library2Entities db = new Library2Entities();
+
+            List<book> list = db.books.Where(
+               c => c.Publisher.Contains(publisher)
+                ).ToList();
+
+            return Json(list);
+        }
+
+        public ActionResult PublisherSearch()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult GetBooksByGenre(string genre)
+        {
+            Library2Entities db = new Library2Entities();
+
+            List<book> list = db.books.Where(
+               c => c.Genre.Contains(genre)
+                ).ToList();
+
+            return Json(list);
+        }
+
+        public ActionResult GenreSearch()
+        {
             return View();
         }
     }
